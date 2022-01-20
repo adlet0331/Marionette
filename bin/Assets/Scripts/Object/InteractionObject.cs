1a6b05c6becd852dab106c9d32bf265cc0232bbc
adlet <adlet@kaist.ac.kr>
2022-01-20 23:58:29 +0900

    InteractionObject 구조 변경
    ScriptableObject에 이어 ItemableObject도 추가하기 위해서 InteractionObject 구조 변경

--- bin/Assets/Scripts/Object/InteractionObject.cs
+++ bin/Assets/Scripts/Object/InteractionObject.cs
@@ -2,25 +2,43 @@
 
 /* Interact 되는 객체
  * 
- * Interact 상속 받아서 사용 (상호작용 함수)
+ * Trigger check 한 Collider에 부딛히면 InteractionList에 추가
  * 
- * idx - 데이터 인덱스
- * InGameObjectManager 에서 데이터 관리
+ * Z 로 Interact 발동
+ * 
  */
 public abstract class InteractionObject : MonoBehaviour
 {
-    // 스크립트가 있는가 (대화창이 뜨는가)
-    [SerializeField] protected bool isScriptable;
-    public void SetisScriptable(bool isSc)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        isScriptable = isSc;
+        if (!isInteractable)
+            return;
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerManager.Instance.playerInteractObject.AddInteractionList(this);
+        }
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerManager.Instance.playerInteractObject.RemoveInteractionObj(this);
+        }
+    }
+    public enum Type
+    {
+        ScriptableObject = 0,
+        ItemableObject = 1,
+    }
+    // 스크립트가 있는가 (대화창이 뜨는가)
+    [SerializeField] public bool isInteractable;
+    [SerializeField] public Type objectType;
     // 데이터 인덱스 (없으면 -1)
     [SerializeField] private int idx;
     public int GetIdx()
     {
-        if (isScriptable)
+        if (isInteractable)
         {
             return idx;
         }
