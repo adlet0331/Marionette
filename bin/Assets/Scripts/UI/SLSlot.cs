3af21cde06779fd261b61cf681bd7f0da13757b3
ray <jw1027@kaist.ac.kr>
2022-07-20 04:15:24 +0900

    SaveLoad UI 뼈대 완성
    이미지는 마땅히 넣을만한게 없어서 안넣음

---
+++ bin/Assets/Scripts/UI/SLSlot.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SLSlot : MonoBehaviour
+{
+    [SerializeField] private GameObject slotSaveNum;
+    [SerializeField] private GameObject slotPlace;
+    [SerializeField] private GameObject slotChapterNum;
+    [SerializeField] private GameObject slotTime;
+    [SerializeField] private GameObject SLImage;
+    [SerializeField] private int slotIndex;
+    
+}
