eb594d874c413a6695d20bf2e188c7a1d4d2d35a
Adlet <eypk777@gmail.com>
2022-08-02 02:47:00 +0900

    Enemy AI 넣어봄 (https://github.com/h8man/NavMeshPlus 추가)

---
+++ bin/Assets/Scripts/InGameObjects/Enemy/EnemyAI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Managers;
+using UnityEngine;
+
+namespace InGameObjects.Enemy
+{
+    public class EnemyAI : MonoBehaviour
+    {
+        private void Start()
+        {
+            UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+            agent.destination = PlayerManager.Instance.moveablePlayerObject.transform.position;
+        }
+    }
+}
