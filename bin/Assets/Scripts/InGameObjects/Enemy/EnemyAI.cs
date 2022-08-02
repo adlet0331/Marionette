6dc61f65bea007b1dd0646283a6c2668cd6c06da
Adlet <eypk777@gmail.com>
2022-08-03 01:51:42 +0900

    적 AI ArtTestScene에 시범적으로 만들어봄

--- bin/Assets/Scripts/InGameObjects/Enemy/EnemyAI.cs
+++ bin/Assets/Scripts/InGameObjects/Enemy/EnemyAI.cs
@@ -6,9 +6,19 @@ namespace InGameObjects.Enemy
 {
     public class EnemyAI : MonoBehaviour
     {
+        [SerializeField] private UnityEngine.AI.NavMeshAgent agent;
+        [SerializeField] private float lastUpdateTime;
         private void Start()
         {
-            UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+            agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+            lastUpdateTime = Time.realtimeSinceStartup;
+        }
+
+        private void Update()
+        {
+            if (Time.realtimeSinceStartup - lastUpdateTime < 0.05)
+                return;
+            lastUpdateTime = Time.realtimeSinceStartup;
             agent.destination = PlayerManager.Instance.moveablePlayerObject.transform.position;
         }
     }
