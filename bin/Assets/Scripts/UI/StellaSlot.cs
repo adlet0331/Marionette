3bf35b618819973480fa7c167c81d268be06df3e
Ray <jw1027@kaist.ac.kr>
2022-08-12 20:55:54 +0900

    StellaSlot Prefab 추가
    ㅁㄴㅇㄺ

---
+++ bin/Assets/Scripts/UI/StellaSlot.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace UI
+{
+    public class StellaSlot : MonoBehaviour
+    {
+        [SerializeField] private Image stellaImage;
+        [SerializeField] private int[] reqExp;
+    }
+}
+
